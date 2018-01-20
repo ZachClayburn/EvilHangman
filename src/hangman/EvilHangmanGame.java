a404d2dd43f7b8943d814a661e0268dd1025d3d9
Zach Clayburn <zachclayburn@gmail.com>
2018-01-19 17:25:14 -0700

    Finished startGame function.

--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -1,9 +1,13 @@
 package hangman;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class EvilHangmanGame implements IEvilHangmanGame{
 
@@ -27,10 +31,6 @@ public class EvilHangmanGame implements IEvilHangmanGame{
 
     }
 
-    public EvilHangmanGame(){
-
-    }
-
     /**
      * Starts a new game of evil hangman using words from <code>dictionary</code>
      * with length <code>wordLength</code>.
@@ -38,12 +38,25 @@ public class EvilHangmanGame implements IEvilHangmanGame{
      * This method should set up everything required to play the game,
      * but should not actually play the game. (ie. There should not be
      * a loop to prompt for input from the user.)
-     *
-     * @param dictionary Dictionary of words to use for the game
+     *  @param dictionaryFile Dictionary of words to use for the game
      * @param wordLength Number of characters in the word to guess
      */
-    public void startGame(File dictionary, int wordLength) {
+    public void startGame(File dictionaryFile, int wordLength) {
+        dictionary = new TreeSet<>();
+        guessList = new TreeSet<>();
 
+        try {//Must Validate dictionaryFile BEFORE passing into this file... unless I come up with a better way
+            Scanner in = new Scanner(dictionaryFile, StandardCharsets.UTF_8.name());
+
+            while (in.hasNext()){
+                String next = in.next();
+                if(next.length() == wordLength){
+                    dictionary.add(next);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            return;
+        }
     }
 
     /**
@@ -57,10 +70,15 @@ public class EvilHangmanGame implements IEvilHangmanGame{
      *                                                    has already been guessed in this game.
      */
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
+        if(!guessList.add(guess)){
+            throw new GuessAlreadyMadeException();
+        }
+
         return null;
     }
 
     //Data Members
     private Set<String> dictionary;
     private Set<Character> guessList;
+    private Set<String> goodGuesses;
 }
